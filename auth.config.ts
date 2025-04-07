2d879366f53706dbd8c655c22d8238ee69c2fcbf
yohei-tarutani <youheitarutani@gmail.com>
2025-04-06 19:13:35 -0700

    add: authentication, metadata etc

--- auth.config.ts
+++ auth.config.ts
@@ -1,19 +1,23 @@
 import type { NextAuthConfig } from "next-auth";
 
-export const authConfig = {
+const authConfig = {
   pages: {
     signIn: "/login",
   },
-  authorized({ auth, request: { nextUrl } }) {
-    const isLoggedIn = !!auth?.user;
-    const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
-    if (isOnDashboard) {
-      if (isLoggedIn) return true;
-      return false; // Redirect unauthenticated users to login page
-    } else if (isLoggedIn) {
-      return Response.redirect(new URL("/dashboard", nextUrl));
-    }
-    return true;
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", nextUrl));
+      }
+      return true;
+    },
   },
   providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
+
+export default authConfig;
